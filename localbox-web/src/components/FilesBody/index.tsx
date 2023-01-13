e5c1822020409f80977ff97ce6d6479f2c4f5de6
Luiz Paulo Pilegi de Almeida <luizpilegilppa@gmail.com>
2023-01-12 21:43:12 -0300

    add new layout to list files

--- localbox-web/src/components/FilesBody/index.tsx
+++ localbox-web/src/components/FilesBody/index.tsx
@@ -1,10 +1,46 @@
 import React from "react";
+import { HiUpload } from "react-icons/hi";
+import { GrAdd } from "react-icons/gr";
 
 import * as S from './styles'
 
 const FilesBody = () => {
+  const tests = [1, 2, 3, 4, 4, 5, 6, 7, 1, 1, 1,
+    1, 2, 3, 4, 4, 5, 6, 7, 1, 1, 1,
+    1, 2, 3, 4, 4, 5, 6, 7, 1, 1, 1]
   return (
-    <h1>FilesBody</h1>
+    <S.ContainerFoles>
+      <S.Options>
+        <S.Button>
+          <HiUpload />
+          Enviar arquivo
+        </S.Button>
+        <S.Button secondary>
+          <GrAdd />
+          Criar Pasta
+        </S.Button>
+      </S.Options>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Nome</th>
+            <th>Proprietário</th>
+            <th>Modificado</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tests.map((i, index) => (
+            <tr key={index}>
+              <td></td>
+              <td>cnh-frente.pdf</td>
+              <td>eu</td>
+              <td>12/09/1995 12:33</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </S.ContainerFoles>
   )
 }
 
--- localbox-web/src/components/FilesBody/styles.ts
+++ localbox-web/src/components/FilesBody/styles.ts
@@ -1,1 +1,56 @@
 import styled from 'styled-components'
+
+export const ContainerFoles = styled.div`
+  overflow-y: auto;
+  padding: 16px;
+  width: 100%;
+
+  table {
+    width: 100%;
+    border-spacing: 0;
+  }
+
+  td, th {
+    padding: 8px;
+    border-bottom: 1px solid hsla(36,23%,55%,.2);
+  }
+
+  th {
+    text-align: start;
+  }
+
+  tbody tr:hover {
+    cursor: pointer;
+    background-color: #f7f5f2;
+  }
+`
+
+export const Options = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 16px;
+
+  button + button {
+    margin-left: 16px;
+  }
+`
+
+type ButtonProp = {
+  secondary?: boolean
+}
+
+export const Button = styled.button<ButtonProp>`
+  height: 40px;
+  border: 0;
+  padding: 8px;
+  display: flex;
+  align-items: center;
+  background-color: #0061fe;
+  color: white;
+  ${({ secondary }) => !!secondary && `background-color: #f7f5f2;`}
+  ${({ secondary }) => !!secondary && `color: #1e1919;`}
+
+  svg {
+    margin-right: 8px;
+  }
+`
